448886ec6020755c4c8e126babcb1d6528d54d58
ivan dinkov <ivdinkov@gmail.com>
2020-06-09 12:35:53 +0100

    disable EnableEndpointRouting for AddMvc in ConfigureServices

--- qs-csharp/Startup.cs
+++ qs-csharp/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace qs_csharp
 {
-	public class Startup
+    public class Startup
     {
         public Startup(IConfiguration configuration)
         {
@@ -26,12 +26,12 @@ namespace qs_csharp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-		}
+            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
